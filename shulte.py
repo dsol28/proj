897cc76e43c14c1c24fd92b5f8ce17bbebc304b5
dsol28 <j8938933@gmail.com>
2024-01-06 17:17:38 +0300

    + game menu

--- shulte.py
+++ shulte.py
@@ -1,7 +1,12 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+)
 import pygame
 import sys
 import random
-pygame.init()
 
 
 class Board:
@@ -13,6 +18,7 @@ class Board:
         self.top = 10
         self.cell_size = 5
         self.finding_num = 1
+        self.ticks = 0
 
     # настройка внешнего вида
     def set_view(self, left, top, cell_size):
@@ -50,6 +56,7 @@ class Shulte(Board):
         super().__init__(width, height)
     
     def spawn(self, surface):
+        global ticks
         sample_of_number = random.sample(range(1, self.width * self.height + 1), self.width * self.height)
         number = 0
         for y in range(self.height):
@@ -57,7 +64,7 @@ class Shulte(Board):
                 self.board[y][x] = sample_of_number[number]
                 font = pygame.font.Font(None, self.cell_size)
                 text = font.render(str(sample_of_number[number]), False, (255, 0, 0))
-                screen.blit(text, (x * self.cell_size, y * self.cell_size))
+                surface.blit(text, (x * self.cell_size, y * self.cell_size))
                 number += 1
         super().render(surface)
 
@@ -66,55 +73,71 @@ class Shulte(Board):
             for x in range(self.width):
                 font = pygame.font.Font(None, self.cell_size)
                 text = font.render(str(self.board[y][x]), False, (pygame.color.Color('black')))
-                screen.blit(text, (self.left + 5 + x * self.cell_size, self.top + 5 + y * self.cell_size))
-        font = pygame.font.Font(None, self.cell_size * 2)
+                surface.blit(text, (self.left + 5 + x * self.cell_size, self.top + 5 + y * self.cell_size))
+        font = pygame.font.Font(None, self.left)
         text = font.render(str(self.finding_num), False, (pygame.color.Color('black')))
-        screen.blit(text, (30, 30))
-        self.time(ticks)
+        surface.blit(text, (self.left - self.cell_size, 30))
+        self.time(self.ticks, surface)
         super().render(surface)
 
     def check(self, cell, surface):
-        global ticks
         if cell:
             y, x = cell
             if self.board[y][x] == self.finding_num:
                 self.finding_num += 1
         if self.finding_num == self.width * self.height + 1:
+            file = open('log.txt', mode='w')
+            file.writelines([str(self.ticks // 60)])
+            file.close()
             self.finding_num = 1
             self.spawn(surface)
-            ticks = 0
+            self.ticks = 0
     
-    def time(self, time):
-        font = pygame.font.Font(None, self.cell_size * 2)
-        text = font.render(str(time // 100), False, (pygame.color.Color('black')))
-        screen.blit(text, (300, 30))
+    def time(self, time, surface):
+        font = pygame.font.Font(None, self.left)
+        text = font.render(str(time // 60), False, (pygame.color.Color('black')))
+        surface.blit(text, (self.width * self.cell_size + self.left, 30))
+
+
+class ShulteMenu(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ShulteMenu.ui', self)
+        self.goButton.clicked.connect(self.startGame)
+        file = open('log.txt', mode='r')
+        info = file.readline()
+        self.lastRezLabel.setText(info)
+        file.close()
+
+    def startGame(self):
+        pygame.init()
+        board = Shulte(self.widthBox.value(), self.heightBox.value())
+        board.set_view(100, 100, 50)
+        width = board.left * 2 + board.width * board.cell_size
+        height = board.top * 2 + board.height * board.cell_size
+        size = width, height
+        screen = pygame.display.set_mode(size)
+        board.spawn(screen)
+        clock = pygame.time.Clock()
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == pygame.BUTTON_LEFT:
+                        board.get_click(event.pos, screen)
+            screen.fill(pygame.color.Color('white'))
+            board.render(screen)
+            clock.tick(60)
+            board.ticks += 1
+            print(board.ticks)
+            pygame.display.flip()
+        pygame.quit()
 
 
 if __name__ == '__main__':
-    
-    board = Shulte(2, 2)
-    board.set_view(100, 100, 50)
-    width = board.left * 2 + board.width * board.cell_size
-    height = board.top * 2 + board.height * board.cell_size
-    size = width, height
-    screen = pygame.display.set_mode(size)
-    board.spawn(screen)
-
-    clock = pygame.time.Clock()
-    ticks = 0
-    
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == pygame.BUTTON_LEFT:
-                    board.get_click(event.pos, screen)
-        screen.fill(pygame.color.Color('white'))
-        board.render(screen)
-        clock.tick(100)
-        ticks += 1
-        pygame.display.flip()
-    pygame.quit()
+    app = QApplication(sys.argv)
+    ex = ShulteMenu()
+    ex.show()
+    sys.exit(app.exec_())
